445250b690c14af961f1ea7faa0d29cef036d097
YunLee9881 <yunjuwon0211@gmail.com>
2024-09-25 20:11:45 +0900

    mention

--- main.rb
+++ main.rb
@@ -1,18 +1,20 @@
-
 # frozen_string_literal: true
+
 require 'discordrb'
 require 'dotenv/load'
 
-file_path = 'data.json'
-file = File.read(file_path)
-data = JSON.parse(file)
-
-bot = Discordrb::Bot.new token: ENV['BOT-TOKEN'], client_id: ENV['CLIENT-ID']
+bot = Discordrb::Bot.new token: ENV['BOT_TOKEN'], client_id: ENV['CLIENT_ID']
 
 bot.message(content: '다한아 뭐해') do |event|
-  m = event.respond('니가 뭔데 알려해')
+  m = event.respond('니가 뭔데 알려해 나 바빠')
   # m.edit "Pong! time taken: #{Time.now - event.timestamp} seconds."
 
 end
+
+bot.mention do |event|
+  event.user.pm('왜 또 뭔데 귀찮게 하지마')
+end
+
+
 
 bot.run
